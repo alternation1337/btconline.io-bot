a7361b7085a11dfa0bbb46d3a0f50dac4f1dd406
alternation1337 <37949005+alternation1337@users.noreply.github.com>
2018-09-03 16:18:12 +0700

    Create btconline.sh
    Special thanks foragusgans
    visit him on link below
    https://github.com/agusgans :)

---
+++ btconline.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+printf "Insert Reff ID: "; read Reff
+reff(){
+ran=$(shuf -i 1-20 -n 1)
+pin=$(shuf -i 1000-9999 -n 1)
+address=$(curl "https://www.blockchain.com/btc/unconfirmed-transactions" -s | grep -Po '(?<=<a href="/btc/address/)[^"]*' | sed -n "$ran"p)
+signup=$(curl -s -X POST -d "wallet_address=$address&pin=$pin&action=post&refid=$Reff" --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:53.0) Gecko/20100101 Firefox/53.0" "https://btconline.io/ajax/sign" -e "https://web.facebook.com/" -x "$proxys" -m 10 2> /dev/null)
+    if [[ $signup == "" ]]
+        then
+   printf "[Address: $address] [Proxy: $proxys]"
+            printf " [Status: Success]\n"
+         else
+printf "[Address: $address] [Proxy: $proxys]"
+            printf " [Status: gagal]\n"
+            fi
+}
+for proxys in $(cat proxylist.txt)
+do
+reff
+done
