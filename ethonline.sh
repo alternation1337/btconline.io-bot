b026df4e296403aacb199dc0b6bf505bbed8238c
alternation1337 <37949005+alternation1337@users.noreply.github.com>
2018-09-03 20:43:36 +0700

    Update ethonline.sh

--- ethonline.sh
+++ ethonline.sh
@@ -4,7 +4,7 @@ printf "Insert Reff ID: "; read Reff
 reff(){
 ran=$(shuf -i 1-50 -n 1)
 pin=$(shuf -i 1000-9999 -n 1)
-address=$(curl "https://blockchair.com/ethereum/transactions/" -s | grep -Po '(?<=<a href="/ethereum/address/)[^"]*' | sed -n "$ran"p)
+$address=(curl "https://blockchair.com/ethereum/transactions/" -s | grep -Po '(?<=<a href="/ethereum/address/)[^"]*' | sed -n "$ran"p)
 signup=$(curl -s -X POST -d "wallet_address=$address&pin=$pin&action=post&refid=$Reff" --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:53.0) Gecko/20100101 Firefox/53.0" "https://ethonline.io/ajax/sign" -e "https://web.facebook.com/" -x "$proxys" -m 10 2> /dev/null)
     if [[ $signup == "" ]]
         then
