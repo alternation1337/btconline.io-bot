699b085bbaaa4113a6d944ca853a03b8379f9aec
Mochammad Lutfi <37949005+alternation1337@users.noreply.github.com>
2018-10-25 14:24:15 +0700

    Update hashmax.sh

--- hashmax.sh
+++ hashmax.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 clear
-printf "Insert Account ID: "; read Reff
-reff(){
+printf "Ref Link: "; read Link
+link(){
 ran=$(shuf -i 1-20 -n 1)
 pin=$(shuf -i 1000-9999 -n 1)
 address=$(curl "https://www.blockchain.com/btc/unconfirmed-transactions" -s | grep -Po '(?<=<a href="/btc/address/)[^"]*' | sed -n "$ran"p)
-signup=$(curl -s -X POST -d "coin_address=$address&password=$pin&action=post&refid=$Reff(https://hashmax.net/r/8997518)" --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:53.0) Gecko/20100101 Firefox/53.0" "https://bitminer.site/auth/register" -e "https://web.facebook.com/" -x "$proxys" -m 10 2> /dev/null)
+signup=$(curl -s -X POST -d "coin_address=$address&password=$pin&action=post&refid=$Link" --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:53.0) Gecko/20100101 Firefox/53.0" "https://bitminer.site/auth/register" -e "https://web.facebook.com/" -x "$proxys" -m 10 2> /dev/null)
     if [[ $signup == "" ]]
         then
    printf "[Address: $address] [Proxy: $proxys]"
@@ -17,5 +17,5 @@ printf "[Address: $address] [Proxy: $proxys]"
 }
 for proxys in $(cat proxylist.txt)
 do
-reff
+link
 done
